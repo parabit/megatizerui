f30cb86059794d53bb26e0185199e55481dc58ca
deniska69 <gostev.dy@yandex.ru>
2025-03-10 17:41:31 +0700

    add config build lib or docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,42 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		react(),
-		tailwindcss(),
-		dts({
-			tsconfigPath: resolve(__dirname, 'tsconfig.lib.json'),
-		}),
-	],
-	build: {
-		lib: {
-			entry: resolve(__dirname, 'lib/main.ts'),
-			formats: ['es'],
-		},
-		copyPublicDir: false,
-		rollupOptions: {
-			external: ['react', 'react/jsx-runtime'],
-		},
-	},
+export default defineConfig(({ mode }) => {
+	if (mode === 'lib') {
+		return {
+			plugins: [
+				react(),
+				tailwindcss(),
+				dts({
+					tsconfigPath: resolve(__dirname, 'tsconfig.lib.json'),
+				}),
+			],
+			build: {
+				lib: {
+					entry: resolve(__dirname, 'lib/main.ts'),
+					formats: ['es'],
+				},
+				outDir: 'dist-lib',
+				copyPublicDir: false,
+				rollupOptions: {
+					external: ['react', 'react/jsx-runtime'],
+				},
+			},
+		};
+	}
+
+	if (mode === 'docs') {
+		return {
+			server: {
+				port: 3000,
+			},
+			preview: {
+				port: 3000,
+			},
+			plugins: [react(), tailwindcss()],
+			build: {
+				outDir: 'dist-docs',
+			},
+		};
+	}
 });
