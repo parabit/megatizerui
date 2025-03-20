005070efbaf21676a5db75ea8f68fe406e41df56
deniska69 <gostev.dy@yandex.ru>
2025-03-20 18:30:17 +0700

    upd divider

--- lib/components/divider/Divider.tsx
+++ lib/components/divider/Divider.tsx
@@ -1,5 +1,7 @@
 import { cn } from '../../utils';
 
 export const Divider = ({ className }: { className?: string }) => (
-	<hr className={cn('border border-card-border-light dark:border-card-border-dark', className)} />
+	<hr
+		className={cn('border-t border-card-border-light dark:border-card-border-dark', className)}
+	/>
 );
