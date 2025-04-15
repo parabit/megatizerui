4bff46c2b3543036589fdddbf612f6ffa06a69cb
deniska69 <gostev.dy@yandex.ru>
2025-04-15 14:21:12 +0700

    decompositing components styles file and upd input styles

--- lib/components/input/Input.tsx
+++ lib/components/input/Input.tsx
@@ -1,13 +1,10 @@
 import { cn } from '../../utils';
 import { Div } from '../div';
-import { IInput, inputDefaultStyle, inputInvalidtStyle } from './utils';
+import { IInput } from './utils';
 
 export const Input = ({ className, isInvalid, rightIcon, ...props }: IInput) => (
 	<Div className={cn('relative w-full', className)}>
-		<input
-			className={cn(inputDefaultStyle, isInvalid && inputInvalidtStyle, className)}
-			{...props}
-		/>
+		<input className={cn('input', isInvalid && 'invalid', className)} {...props} />
 		{rightIcon}
 	</Div>
 );
--- lib/components/input/utils.ts
+++ lib/components/input/utils.ts
@@ -1,10 +1,5 @@
 import { InputHTMLAttributes, ReactNode, Ref, RefObject } from 'react';
 
-export const inputDefaultStyle =
-	'block w-full rounded-lg border border-[#e5e7eb] bg-neutral-100 px-4 py-3 text-sm placeholder:text-neutral-500 focus:border-neutral-400 dark:border-neutral-700 dark:bg-neutral-900';
-
-export const inputInvalidtStyle = '!border-red-500 focus:!border-red-500 dark:!border-red-500';
-
 export interface IInput extends InputHTMLAttributes<HTMLInputElement> {
 	ref?: Ref<HTMLInputElement | null>;
 	className?: string;
