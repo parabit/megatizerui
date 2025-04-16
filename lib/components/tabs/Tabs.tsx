631a9cc6fdf44be86bdb81d88bb97f8b1efb988d
deniska69 <gostev.dy@yandex.ru>
2025-04-16 15:19:44 +0700

    upd tabs styles

--- lib/components/tabs/Tabs.tsx
+++ lib/components/tabs/Tabs.tsx
@@ -1,7 +1,6 @@
 import { Children, cloneElement } from 'react';
 
 import { cn } from '../../utils';
-import { Button } from '../button';
 import { Div } from '../div';
 import { HStack } from '../hstack';
 import { Stack } from '../stack';
@@ -14,34 +13,14 @@ const Tab = (props: ITab) => {
 	};
 
 	return (
-		<Button
-			variant="ghost"
-			onClick={handleChange}
-			className={cn(
-				'!py-1 w-full text-nowrap',
-
-				props.active ? '!bg-card-bg-light' : '!bg-neutral-200', //bg light
-				props.active ? '!text-neutral-600' : '!text-neutral-400', //text light
-
-				props.active ? 'dark:!bg-neutral-600' : 'dark:!bg-neutral-800', //bg dark
-				props.active ? 'dark:!text-neutral-200' : 'dark:!text-neutral-500', //text dark
-
-				props.active ? 'hover:!text-neutral-900' : 'hover:!text-neutral-500', // hover text
-				props.active ? 'dark:hover:!text-neutral-400' : 'dark:hover:!text-neutral-400', // hover text dark
-			)}
-		>
+		<button onClick={handleChange} className={props.active ? 'tab active' : 'tab'}>
 			{props.children}
-		</Button>
+		</button>
 	);
 };
 
 const TabList = (props: ITabList) => (
-	<HStack
-		className={cn(
-			'dark:bg-neutral-800 gap-x-1 rounded-xl bg-neutral-200 p-0.5 w-fit',
-			props.className,
-		)}
-	>
+	<HStack className={cn('tab-list', props.className)}>
 		{Array.isArray(props.children) &&
 			Children.map(props.children, (child, index) =>
 				cloneElement(child, {
@@ -68,7 +47,7 @@ const PanelList = (props: IPanelList) => (
 );
 
 const Tabs = (props: ITabs) => (
-	<Stack className={cn('gap-y-3 w-auto', props.className)}>{props.children}</Stack>
+	<Stack className={cn('tabs', props.className)}>{props.children}</Stack>
 );
 
 Tabs.TabList = TabList;
